aa11e24bd77f68b9e54af39f2d99e67d3991aa9b
Masayoshi Wada <developer@andantesoftware.com>
2015-04-21 20:30:18 +0900

    browserSync使えるようにした

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,38 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserify = require('browserify');
+var browserSync = require('browser-sync');
 var source = require('vinyl-source-stream');
 
+gulp.task('server', ['watch', 'build'], function() {
+  browserSync({
+    server: {
+      baseDir: 'build'
+    }
+  });
+});
+
 gulp.task('build', function() {
-  browserify({entries: ['./src/app.js']})
+  browserify({entries: ['./src/js/app.js']})
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/*.js', ['build']);
+  gulp.watch('./src/js/*.js', ['build']);
+  gulp.watch('./build/**', ['reload']);
+  gulp.watch('./src/sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/sass/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('default', ['server']);
